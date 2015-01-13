3687f9a7936139b2c9cb1e4850f1dcb32a6278c5
Mohamed Refaat <eltntawy@gmail.com>
2015-01-14 01:02:14 +0200

    finish lab 7

--- src/standard/lab7/TowList.java
+++ src/standard/lab7/TowList.java
@@ -14,6 +14,9 @@ import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.ListSelectionModel;
 
 public class TowList extends JFrame implements ActionListener {
 
@@ -34,41 +37,19 @@ public class TowList extends JFrame implements ActionListener {
 		// TODO Auto-generated method stub
 		super("Tow List");
 
-		setSize(500, 300);
+		setSize(600, 350);
 
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-
-		getContentPane().setLayout(new BorderLayout());
 		init();
 		setVisible(true);
 	}
 
 	void init() {
 
-		btnAdd = new JButton(">");
-		btnAdd.setMinimumSize(new Dimension(50, 50));
-		btnRemove = new JButton("<");
-		btnRemove.setMinimumSize(new Dimension(50, 50));
-		btnAddAll = new JButton(">>");
-		btnAddAll.setMinimumSize(new Dimension(50, 50));
-		btnRemoveAll = new JButton("<<");
-		btnRemoveAll.setMinimumSize(new Dimension(50, 50));
-
-		btnAdd.addActionListener(this);
-		btnRemove.addActionListener(this);
-		btnAddAll.addActionListener(this);
-		btnRemoveAll.addActionListener(this);
-
-		toolPanel = new JPanel();
-		toolPanel.setLayout(new BoxLayout(toolPanel, BoxLayout.Y_AXIS));
-
-		centerPanel = new JPanel();
-		centerPanel.setLayout(new FlowLayout());
-
-		centerPanel.add(toolPanel);
-
 		list1 = new JList<String>();
+		list1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		list2 = new JList<String>();
+		list2.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		MyListModel<String> list1Model = new MyListModel<String>();
 
 		list1Model.addElement("Element 1");
@@ -77,7 +58,7 @@ public class TowList extends JFrame implements ActionListener {
 		list1Model.addElement("Element 4");
 		list1Model.addElement("Element 5");
 		list1Model.addElement("Element 6");
-		
+
 		MyListModel<String> list2Model = new MyListModel<String>();
 		list2Model.addElement("Element 1");
 		list2Model.addElement("Element 2");
@@ -88,52 +69,100 @@ public class TowList extends JFrame implements ActionListener {
 
 		list1.setModel(list1Model);
 		list2.setModel(list2Model);
+		
+		//list1.setPrototypeCellValue("12345678901234567890"); 
+		//list2.setPrototypeCellValue("12345678901234567890"); 
 
 		list1Pane = new JScrollPane();
+		list1Pane.setViewportBorder(new LineBorder(new Color(0, 0, 0), 2, true));
 		list2Pane = new JScrollPane();
+		list2Pane.setViewportBorder(new LineBorder(new Color(0, 0, 0), 2, true));
 
-		list1Pane.setSize(100, 200);
-		list2Pane.setSize(100, 200);
+		list1Pane.setPreferredSize(new Dimension(200,300));
+		list2Pane.setPreferredSize(new Dimension(200,300));
+		
+		
 
-		list1Pane.add(list1);
-		list2Pane.add(list2);
-
-		toolPanel.add(btnAddAll);
-		toolPanel.add(btnAdd);
-		toolPanel.add(btnRemove);
-		toolPanel.add(btnRemoveAll);
-
-		add(centerPanel, BorderLayout.CENTER);
-		add(list1, BorderLayout.WEST);
-		add(list2, BorderLayout.EAST);
+		list1Pane.setViewportView(list1);
+		list2Pane.setViewportView(list2);
+		getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		getContentPane().add(list1Pane);
+		
+				btnAdd = new JButton(">");
+				btnAdd.setMinimumSize(new Dimension(50, 50));
+				btnRemove = new JButton("<");
+				btnRemove.setMinimumSize(new Dimension(50, 50));
+				btnAddAll = new JButton(">>");
+				btnAddAll.setMinimumSize(new Dimension(50, 50));
+				btnRemoveAll = new JButton("<<");
+				btnRemoveAll.setMinimumSize(new Dimension(50, 50));
+				
+						btnAdd.addActionListener(this);
+						btnRemove.addActionListener(this);
+						btnAddAll.addActionListener(this);
+						btnRemoveAll.addActionListener(this);
+						
+								toolPanel = new JPanel();
+								toolPanel.setLayout(new BoxLayout(toolPanel, BoxLayout.Y_AXIS));
+								
+										centerPanel = new JPanel();
+										centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.X_AXIS));
+										
+												centerPanel.add(toolPanel);
+												
+														toolPanel.add(btnAddAll);
+														toolPanel.add(btnAdd);
+														toolPanel.add(btnRemove);
+														toolPanel.add(btnRemoveAll);
+														
+																getContentPane().add(centerPanel);
+		getContentPane().add(list2Pane);
 
 	}
 
 	public void moveElementFromTo(JList<String> listFrom, JList<String> listTo) {
 
-		String temp = listFrom.getSelectedValue();
-		((MyListModel<String>) (listFrom.getModel())).removeElement(listFrom.getSelectedIndex());
+		if (0 <= listFrom.getSelectedIndex()  && listFrom.getSelectedIndex() < listFrom.getModel().getSize()) {
+			String temp = listFrom.getSelectedValue();
+			((MyListModel<String>) (listFrom.getModel()))
+					.removeElement(listFrom.getSelectedIndex());
 
-		((MyListModel<String>) listTo.getModel()).addElement(temp);
+			((MyListModel<String>) listTo.getModel()).addElement(temp);
 
+			MyListModel<String> tempModel = new MyListModel<String>();
+			for (String s : ((MyListModel<String>) listTo.getModel()).dataVector) {
+				tempModel.addElement(s);
+			}
+
+			listTo.setModel(tempModel);
+
+		}
 	}
 
 	public void moveAllElementFromTo(JList<String> listFrom,
 			JList<String> listTo) {
 
+		 
 		MyListModel<String> tempModel = new MyListModel<String>();
 		for (String s : ((MyListModel<String>) listFrom.getModel()).dataVector) {
-			tempModel.addElement(s);
+			((MyListModel<String> ) listTo.getModel()).addElement(s);
 		}
-
-		listTo.setModel(tempModel);
+		
+		for (String s : ((MyListModel<String>) listTo.getModel()).dataVector) {
+			
+			tempModel.addElement(s);
+			
+		}
+		
 		((MyListModel<String>) listFrom.getModel()).removeAll();
+		listTo.setModel(tempModel);
 
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+
 		if (e.getSource() == btnAdd) {
 			moveElementFromTo(list1, list2);
 		} else if (e.getSource() == btnRemove) {
@@ -182,10 +211,10 @@ public class TowList extends JFrame implements ActionListener {
 		@Override
 		public String toString() {
 			// TODO Auto-generated method stub
-			
-			String ret = "" ;
-			for(E s : dataVector ) {
-				ret+=s.toString()+"\n";
+
+			String ret = "";
+			for (E s : dataVector) {
+				ret += s.toString() + "\n";
 			}
 			return ret;
 		}
@@ -195,5 +224,4 @@ public class TowList extends JFrame implements ActionListener {
 		// TODO Auto-generated method stub
 		new TowList();
 	}
-
 }
